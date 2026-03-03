193e10a48d2190b6556bb2acf8319e39618f78d4
Aksyme ./ <wwznoz66f@gmail.com>
2026-03-03 10:53:58 +0200

    docs: add run instructions and improve example

--- src/main.js
+++ src/main.js
@@ -2,3 +2,6 @@ console.log('Hello world!');
 
 const sum = (a, b) => a + b;
 console.log('Сума 10 + 20 =', sum(10, 20));
+
+const greet = (name) => `Привіт, ${name}!`;
+console.log(greet('Студент'));
